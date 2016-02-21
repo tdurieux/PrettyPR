b85ba87ed5dd79a0198a72e4599026912a22b4e5
Nicolas D <nicolas_delperdange@hotmail.com>
2016-02-21 15:59:52 +0100

    Ajout de tests et modification pour travis

---
+++ tests/components/models/test.js
@@ -0,0 +1,8 @@
+describe('Chimp Mocha', function() {
+  describe('Page title', function () {
+    it('should be set by the Meteor method @watch', function () {
+      browser.url('http://www.google.com');
+      expect(browser.getTitle()).to.equal('Google');
+    });
+  });
+});
