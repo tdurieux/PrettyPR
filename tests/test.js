2d0ab085feab1ec2600235716e764733ea0f4096
Nicolas Delperdange <nicolas.delperdange@etudiant.unif-lille1.fr>
2016-02-26 14:09:43 +0100

    Test Travis

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+//tests/posts.js
+var assert = require('assert');
+
+suite('Posts', function() {
+  test('in the server', function(done, server) {
+    server.eval(function() {
+      Posts.insert({title: 'hello title'});
+      var docs = Posts.find().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs.length, 1);
+      done();
+    });
+  });
+});
